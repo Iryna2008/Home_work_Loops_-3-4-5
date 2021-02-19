cb6a8ca6308abe25ed4f236f5facc07d9e2e9739
DESKTOP-8MOEEPG\Део <gonch.irch@gmail.com>
2021-02-19 22:24:48 +0200

    first commit

---
+++ Home_work_Loops_3-4/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Home_work_Loops_3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //#3
+            //  *
+            // **
+            //***
+            Console.WriteLine("Enter rows count");
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int y = 0; y < n; y++)
+            {
+                for (int x = n; x > y; x--)
+                {
+                    Console.Write(" ");
+                }
+                for (int x = 0; x <= y; x++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            //№ 4
+            //***
+            //**
+            //*
+            Console.WriteLine("Enter rows count");
+
+            int c = int.Parse(Console.ReadLine());
+
+            for (int d = 0; d < c; d++)
+            {
+
+                for (int x = c; x > d; x--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            //№ 5
+            //***
+            // **
+            //  *
+            Console.WriteLine("Enter rows count");
+
+            int f = int.Parse(Console.ReadLine());
+
+            for (int y = 0; y < f; y++)
+            {
+
+                for (int x = 0; x <= y; x++)
+                {
+                    Console.Write(" ");
+                }
+                for (int x = f; x > y; x--)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+                                
+            }
+        }
+    }
+}
